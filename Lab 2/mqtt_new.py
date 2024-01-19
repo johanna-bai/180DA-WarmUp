8bf4e97ac7dbccf42df1563ae4ad16482523bfd4
Johanna <johannabai102@ucla.edu>
2024-01-18 17:48:26 -0800

    lab 2 in progress

---
+++ Lab 2/mqtt_new.py
@@ -0,0 +1,59 @@
+import paho.mqtt.client as mqtt
+import time
+import numpy as np
+
+# 0. define callbacks - functions that run when events happen.
+# The callback for when the client receives a CONNACK response from the server.
+def on_connect(client, userdata, flags, rc):
+    print("Connection returned result: " + str(rc))
+    client.subscribe("ece180d/test/team6/#", qos=1)
+    client.publish("ece180d/test/team6/1", "hi this is johanna", qos=1)
+
+# Subscribing in on_connect() means that if we lose the connection and
+# reconnect then subscriptions will be renewed.
+# client.subscribe("ece180d/test")
+# The callback of the client when it disconnects.
+def on_disconnect(client, userdata, rc):
+    if rc != 0:
+        print('Unexpected Disconnect')
+    else:
+        print('Expected Disconnect')
+# The default message callback.
+# (won’t be used if only publishing, but can still exist)
+def on_message(client, userdata, message):
+    time.sleep(1)
+    #print(message.topic)
+    topic = int(message.topic.split('/')[-1])
+    if topic != 1:
+        print('Received message: "' + str(message.payload) + '" on topic "' + message.topic + '" with QoS ' + str(message.qos))
+        print('Publishing...')
+        client.publish("ece180d/test/team6/1", "received" , qos=1)
+
+# 1. create a client instance.
+client = mqtt.Client()
+# add additional client options (security, certifications, etc.)
+# many default options should be good to start off.
+# add callbacks to client.
+client.on_connect = on_connect
+client.on_disconnect = on_disconnect
+client.on_message = on_message
+
+# 2. connect to a broker using one of the connect*() functions.
+# client.connect_async("test.mosquitto.org")
+client.connect_async('mqtt.eclipseprojects.io')
+
+# 3. call one of the loop*() functions to maintain network traffic flow with the broker.
+client.loop_start()
+#client.publish("ece180d/test/team6", "hi this is johanna", qos=1)
+
+# 4. use subscribe() to subscribe to a topic and receive messages.
+while True: 
+    pass
+
+
+# 5. use publish() to publish messages to the broker.
+# payload must be a string, bytearray, int, float or None.
+
+# 6. use disconnect() to disconnect from the broker.
+client.loop_stop()
+client.disconnect()
